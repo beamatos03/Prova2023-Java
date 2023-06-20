94b2adf6722c9e7fc8d5665c789f183009f554ff
beamatos03 <bea.matos1978@gmail.com>
2023-06-20 11:03:27 -0300

    listar

--- src/main/java/com/fatec/empresa/resources/EmpresaController.java
+++ src/main/java/com/fatec/empresa/resources/EmpresaController.java
@@ -1,10 +1,12 @@
 package com.fatec.empresa.resources;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,22 +21,29 @@ import com.fatec.empresa.services.EmpresaService;
 @RestController
 public class EmpresaController {
 
-
     @Autowired
     private EmpresaService empresaService;
 
     @PostMapping
-    public ResponseEntity<Empresa> saveClient(@RequestBody Empresa ent){
+    public ResponseEntity<Empresa> saveClient(@RequestBody Empresa empresa){
 
-        Empresa newEmpresa = empresaService.save(ent);
+        Empresa newEmpresa = empresaService.save(empresa);
         URI location = ServletUriComponentsBuilder
         .fromCurrentRequest()
         .path("/{id}")
-        .buildAndExpand(ent.getId())
+        .buildAndExpand(empresa.getId())
         .toUri();
         
         return ResponseEntity.created(location).body(newEmpresa);
     }
+
+    @GetMapping
+    public ResponseEntity<List<Empresa>> getEmpresas() {
+        List<Empresa> empresas = empresaService.getEmpresas();
+        return ResponseEntity.ok().body(empresas);
+    }
+
+
 
     
 }
